8eafe83f37ccc70b672b22f75baae980a300398d
DarkLyng <darlyng.rojas@ucr.ac.cr>
2025-08-01 00:09:00 -0600

    Áctualiza el app.js

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let listaNombres = [];
 
+function asignarTextoElemento(texto, elemento) {
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return console.log(elementoHTML);
+}
 
-
-function agregarNombreAmigoSecreto(lista) {
-    let 
-
+function agregarAmigo(lista, texto) {
+    let nombreAmigo = document.getElementById('listaAmigos');
+    return console.log(nombreAmigo);
 }
 
 
-function validarNombreAmigoSecreto(lista) {
-    
+function validarNombreAmigo(lista) {
+    if (nombreAmigo == "") {
+        asignarTextoElemento('p','La casilla está vacía, por favor ingrese un nombre');
+    } else {
+
+    }
 }
 
 
@@ -18,22 +26,22 @@ function limpiarcasilla() {
     document.querySelector('#amigo').value = '';
 }
 
-function visualizarListaAmigoSecreto(lista) { 
+function visualizarListaAmigo(lista) { 
 
 }
 
-function sortearAmigoSecreto(lista) {
+function sortearAmigo(lista) {
     let sorteoRandom = listaNombres
 }
 
 
 
-//Ingresar un nombre en una lista
-//verificar la casilla no esté vacía
-//verificar que no se repita el nombre
+//pide un nombre y se ingresa en una lista
+//verificar la casilla no esté vacía, si está vacía o sale un número sale mensaje "Ingrese un nombre", si no agrega el nombre a la lista
+//verificar que no se repita el nombre, si se repite, dice "Nombre repetido, ingrese otro", si no continua
 //guardarlo en una lista
 //limpiar la casilla
 //agregar una función en donde aparezcan todos los nombres ingresados abajo 
 //Repetir el proceso hasta que se seleccione 'sortear amigo sercreto'
 //seleccionar de forma aleatoria un nombre entre la lista de amigos y mostrarlo
-//Luego de seleccionarlo repertir todo
+//Luego de seleccionarlo repertir todo el proceso
