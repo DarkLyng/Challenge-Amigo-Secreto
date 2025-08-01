a39e1bd4d8735e118dd257f492c4ed435cb41f0e
DarkLyng <darlyng.rojas@ucr.ac.cr>
2025-07-31 19:58:49 -0600

    Actualiza el app.js

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaNombres = [];
+
+
+
+function agregarNombreAmigoSecreto(lista) {
+    listaNombres = p
+
+}
+
+function validarNombreAmigoSecreto(lista) {
+
+}
+
+
+function visualizarListaAmigoSecreto(lista) { 
+
+}
+
+
+function sortearAmigoSecreto(lista) {
+    let sorteoRandom = listaNombres
+}
