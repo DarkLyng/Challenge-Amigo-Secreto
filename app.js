bf3455733ad12c0badcc167065bc780594d6b460
DarkLyng <darlyng.rojas@ucr.ac.cr>
2025-08-02 02:00:04 -0600

    Actualiza el app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let listaNombres = [];
+let nombresPorSortear = [];
 
 function asignarTextoElemento(elemento, texto) {
     let elementoHTML = document.querySelector(elemento);
@@ -11,34 +12,27 @@ function agregarAmigo() {
     let nombreAmigo = document.getElementById('amigo').value;
     if (nombreAmigo === "") {
         asignarTextoElemento('h3','La casilla está vacía, por favor ingrese un nombre.');
+    
+    } else if (nombreAmigo == parseInt(nombreAmigo)) {
+        asignarTextoElemento('h3','Ingrese un nombre, no un número.');
         limpiarcasilla();
-        return;
-    }
-    let nombreMinuscula = nombreAmigo.toLowerCase();
-    if (listaNombres.includes(nombreMinuscula)){
-        asignarTextoElemento('h3','Ese nombre ya se ingreso, por favor ingrese otro nombre.');
+    
     } else {
-        listaNombres.push(nombreMinuscula);
-        asignarTextoElemento('h3','');
-        limpiarcasilla();
-
-        }
-        return console.log(listaNombres);
+        let nombreMinuscula = nombreAmigo.toLowerCase();
+        
+        if (listaNombres.includes(nombreMinuscula)){
+            asignarTextoElemento('h3','Ese nombre ya se ingreso, por favor ingrese otro nombre.');
+        
+        } else {
+            listaNombres.push(nombreMinuscula);
+            document.getElementById('listaAmigos').innerHTML = "";
+            asignarTextoElemento('h3','');
+            limpiarcasilla();
+            visualizarListaAmigo(listaNombres);
+            }
     }
-
-function evitarNumeros(){
-    if (nombreAmigo === parseInt(nombreAmigo) ){
-        asignarTextoElemento('h3','Ingrese un nombre, no un número.')
-        return;
-
-    } else {
-        agregarAmigo();
-        limpiarcasilla();
-        return visualizarListaAmigo(listaNombres);
-    }
+    return console.log(listaNombres);
 }
-
-
 
 function limpiarcasilla() {
     document.getElementById('amigo').value ='';
@@ -46,14 +40,33 @@ function limpiarcasilla() {
 }
 
 function visualizarListaAmigo(lista) { 
-
+    for (let i =0; i < listaNombres.length; i++){
+            document.getElementById('listaAmigos').innerHTML += `<li>${listaNombres[i]}</li>`;
+        }
+    return ;
 }
 
 function sortearAmigo(lista) {
-    let sorteoRandom = listaNombres
+    if (listaNombres.length === 0 || listaNombres.length === 1) {
+        document.getElementById('resultado').innerHTML = 'No se puede realizar el sorteo, por favor ingrese al menos dos nombres';
+    } else { 
+        let amigoRandom = Math.floor(Math.random()*listaNombres.length);
+        document.getElementById('resultado').innerHTML = `El amigo secreto es ${listaNombres[amigoRandom]}`;
+    }
+    return;
 }
 
+// function condicionesIniciales() {
+    // document.getElementById('listaAmigos').innerHTML = '';
+    // document.getElementById('resultado').innerHTML = '';
+    // asignarTextoElemento('h3','');
+    // limpiarcasilla();
+    // listaNombres = [];
+    // document.getElementById('amigo').value = '';
+    
+// }
 
+// condicionesIniciales();
 
 //pide un nombre y se ingresa en una lista
 //verificar la casilla no esté vacía, si está vacía o sale un número sale mensaje "Ingrese un nombre", si no agrega el nombre a la lista
